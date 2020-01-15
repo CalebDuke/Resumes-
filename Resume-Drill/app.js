c8decbc6204dbcfadf1dc38e29e87c9cf48a327d
CalebDuke <59664257+CalebDuke@users.noreply.github.com>
2020-01-15 16:53:01 -0600

    Add files via upload

---
+++ Resume-Drill/app.js
@@ -0,0 +1,11 @@
+console.log('Caleb Duke');
+
+console.log('Web Developer');
+
+console.log('I like eatin foods');
+
+console.log('Food', 'Music', 'Nicki Rakestraw');
+
+console.log()
+
+console.log()
