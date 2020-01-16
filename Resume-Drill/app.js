1219754a3323968d42b308ece2c5c6bae51768f9
CalebDuke <59664257+CalebDuke@users.noreply.github.com>
2020-01-16 11:02:03 -0600

    Add files via upload

--- Resume-Drill/app.js
+++ Resume-Drill/app.js
@@ -1,11 +1,44 @@
-console.log('Caleb Duke');
+var str = "Caleb Duke";
+var res = str.toLocaleUpperCase();
 
-console.log('Web Developer');
+function displayPosition() {
+    console.log("Name: " + res);
+    console.log("Career: " + jt);
+    console.log("Description: " + des);
+}
 
-console.log('I like eatin foods');
+var jt = "Web Developer";
+
+var des = "I like eatin foods";
+
+displayPosition()
 
 console.log('Food', 'Music', 'Nicki Rakestraw');
 
-console.log()
+console.log(
+"* Math, Chemistry, Physics, Biology, and English tutor at Some School - Tutored students in those areas.\n",
+"* Undergraduate Teaching Assistant at University of Georgia - Helped grade assignments, answer questions, held lab hours, and held office hours for Java and C++ Computer Science classes.\n",
+"* Lead Programmer for iBeacon Experiment at Georgia Museum of Art - Created native iOS virtual tour guide that used iBeacon technology to triangulate position within the museum and give information about a piece.");
 
-console.log()
+// var skills = {
+//    a: "French",
+//    b: "BAM: Trombone",
+//    c: "Java",
+//    d: "C / C++",
+//    e: "BAM: Objective-C / Swift",
+// }
+
+    function displaySkill(skill, isCool){
+        if (isCool == true){
+            console.log("* BAM: " + skill)
+        } else {
+            console.log("* " + skill)
+        }
+    }
+    console.log("My Skills:")
+    
+    displaySkill("French", false)
+    displaySkill("Trombone", true)
+    displaySkill("Java", false)
+    displaySkill("C / C++", false)
+    displaySkill("Objective-C / Swift", true)
